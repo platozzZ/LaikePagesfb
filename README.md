26cb4d2d7cb7b863910a7388a562d338221d9694
platozzZ <779667085@qq.com>
2020-09-25 11:46:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LaikePagesfb
+ 
---
+++ pages/jump/jumpToFb.vue
@@ -0,0 +1,16 @@
+<template>
+    	<view class="my">
+            请稍等，跳转中。。。
+    	</view>
+</template>
+<script>
+export default {
+    data() {
+        return {};
+    },
+    onLoad(options) {
+        //todo 配置
+       window.location.href="http://sns.yqqedu.com/#"
+    }
+}
+</script>
